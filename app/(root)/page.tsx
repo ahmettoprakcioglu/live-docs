1d449771c02ed2889a1f6d5bebb741d5521c30e1
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2025-01-05 00:46:55 +0300

    feat: enhance home page with user authentication, document handling, and collaborative features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from '@/components/ui/button'
+import AddDocumentBtn from '@/components/AddDocumentBtn'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect(process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL as string);
+
+  const documents = [];
   return (
-    <div className='p-10'>
-      <Button>Click Me</Button>
-    </div>
+    <main className='home-container'>
+      <Header className='sticky left-0 top-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div>adad</div>
+      ) : (
+        <div className='document-list-empty'>
+          <Image
+            src='assets/icons/doc.svg'
+            alt='Document'
+            width={40}
+            height={40}
+            className='mx-auto'
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   )
 }
 
-export default page
+export default Home
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Header = ({
-  children
+  children,
+  className
 }: HeaderProps) => {
   return (
-    <div className='header'>
+    <div className={cn('header', className)}>
       <Link href="/" className='md: flex-1'>
         <Image
           src='/assets/icons/logo.svg'
